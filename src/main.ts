47b8e40a79a8770285248cd6edd05c4e4402c2c1
Bohdan Bulakh <bgdn.56t@gmail.com>
2024-06-10 00:43:29 +0300

    enhancement: cleaned project
    - deleted unnecessary files
    - customised main ts file

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { AppModule } from './v1/app.module';
+import * as process from 'process';
 
-async function bootstrap() {
+
+async function bootstrap () {
+  const port = process.env.PORT ?? 3000;
+
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(port, () => console.info(`App listening on 127.0.0.1:${port}`));
 }
 bootstrap();
